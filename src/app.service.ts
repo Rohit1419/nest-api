bd92580a29890fab67b8a5153c500bc662ebf4df
Rohit Gite <rohitgite1419@gmail.com>
2025-12-08 01:11:00 +0530

    update greeting message in AppService to encourage user engagement

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello !';
+    return 'If you see this, then I deserve a Star !';
   }
 }
