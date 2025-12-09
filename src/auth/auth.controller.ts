758aea2f3f64c47f6d052f814b878f4697f904d6
Rohit Gite <rohitgite1419@gmail.com>
2025-12-10 00:09:10 +0530

    created the jwt accestoken provider

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,13 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
-import { dot } from 'node:test/reporters';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signUp(@Body() dto: AuthDto) {
+  async signUp(@Body() dto: AuthDto) {
     console.log('DTO ', dto);
     return this.authService.signUp(dto);
   }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { Prisma, Users } from '../../generated/prisma/client';
 import * as argon from 'argon2';
 import { AuthDto } from './dto';
+import { JwtService } from '@nestjs/jwt';
+import { access } from 'fs';
+import { sign } from 'crypto';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
 
   // signup logic here
   async signUp(dto: AuthDto) {
@@ -28,15 +34,25 @@ export class AuthService {
         },
       });
 
-      return user;
+      return this.signToken(user.id, user.email);
     } catch (error) {
       console.log('error ', error);
       throw error;
     }
   }
 
-  async signIn() {
-    const user = await this.prisma.users.findMany({});
-    return user;
+  // sign a token generater for the user
+
+  async signToken(userID: number, email: string): Promise<string> {
+    const payload = { sub: userID, email };
+
+    const accessToken: string = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: process.env.JWT_SECRET,
+    });
+
+    return {
+      access_token: accessToken,
+    };
   }
 }
