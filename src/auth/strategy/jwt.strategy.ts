3bbad4a5af139a00497e693ceae136b4d6bfc2af
Rohit Gite <rohitgite1419@gmail.com>
2025-12-13 23:51:33 +0530

    refactor JwtStrategy validate method to return null instead of payload

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -14,6 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 
   async validate(payload: any) {
     console.log('payload', payload);
-    return payload;
+    return null;
   }
 }
